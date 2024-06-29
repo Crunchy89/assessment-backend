bf01dc97710c10ca514232e639424bda0a425ac1
crunchy89 <rocker.hunt@gmail.com>
2024-06-29 20:54:37 +0800

    update swagger not include on production

--- dist/main.js
+++ dist/main.js
@@ -5,19 +5,23 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const http_exception_filter_1 = require("./http-exception.filter");
 const swagger_1 = require("@nestjs/swagger");
+const dotenv = require("dotenv");
 async function bootstrap() {
+    dotenv.config();
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
     app.useGlobalFilters(new http_exception_filter_1.HttpExceptionFilter());
-    const config = new swagger_1.DocumentBuilder()
-        .setTitle('Example API')
-        .setDescription('The example API description')
-        .setVersion('1.0')
-        .addTag('example')
-        .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'access-token')
-        .build();
-    const document = swagger_1.SwaggerModule.createDocument(app, config);
-    swagger_1.SwaggerModule.setup('api', app, document);
+    if (process.env.NODE_ENV !== 'production') {
+        const config = new swagger_1.DocumentBuilder()
+            .setTitle('Example API')
+            .setDescription('The example API description')
+            .setVersion('1.0')
+            .addTag('example')
+            .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'access-token')
+            .build();
+        const document = swagger_1.SwaggerModule.createDocument(app, config);
+        swagger_1.SwaggerModule.setup('api', app, document);
+    }
     await app.listen(3000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'src/app.module';
 import { HttpExceptionFilter } from 'src/http-exception.filter';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
-  const config = new DocumentBuilder()
-    .setTitle('Example API')
-    .setDescription('The example API description')
-    .setVersion('1.0')
-    .addTag('example')
-    .addBearerAuth( // This configures Swagger to use Bearer Auth
-      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
-      'access-token', // This is the name used internally by Swagger UI
-    )
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Example API')
+      .setDescription('The example API description')
+      .setVersion('1.0')
+      .addTag('example')
+      .addBearerAuth( // This configures Swagger to use Bearer Auth
+        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+        'access-token', // This is the name used internally by Swagger UI
+      )
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
   await app.listen(3000);
 }
 bootstrap();
